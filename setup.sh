ce950613089cedb81faa57311dc181e1892cc2a0
Artemiy Vanskiy <a.vanskiy@life-stream.tv>
2018-10-16 15:33:43 +0300

    Fixes #27396 Add python script for changing venv/bin/activate

--- setup.sh
+++ setup.sh
@@ -21,10 +21,7 @@ echo "Starting setup reqirments for addon to venv"
 
 pip3 install -r ../requirements.txt
 
-sed -i -e 's/^deactivate () {$/deactivate () {\n if [ -n "${_OLD_VIRTUAL_PYTHONPATH:-}" ] ;\n    then PYTHONPATH="${_OLD_VIRTUAL_PYTHONPATH:-}"\n    export PYTHONPATH\n    unset _OLD_VIRTUAL_PYTHONPATH\n fi/' $VIRTUAL_ENV/bin/activate
-
-echo 'export _OLD_VIRTUAL_PYTHONPATH="$PYTHONPATH"
-export PYTHONPATH="$VIRTUAL_ENV/lib/python3.6/site-packages:$PYTHONPATH"' >> $VIRTUAL_ENV/bin/activate
+python3 ../add_pythonpath.py
 
 export PYTHON_PROTO_PATH="$VIRTUAL_ENV/lib/python3.6/site-packages/proto_py"
 
@@ -32,7 +29,6 @@ cd ../frontendproto
 
 mkdir -p $PYTHON_PROTO_PATH
 protoc --proto_path . --python_out=$PYTHON_PROTO_PATH $(find .  -type f -name '*.proto')
-
 set -
 
 echo "Installation is finished"
