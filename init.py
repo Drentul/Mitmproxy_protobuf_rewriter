fb68e0a822b4a215c5424535509350634f758fbc
Artemiy Vanskiy <a.vanskiy@life-stream.tv>
2019-06-20 14:39:25 +0300

    Fixes the init.py filename so that the project root folder is not marked as a python module.

--- init.py
+++ init.py
@@ -1,10 +1,13 @@
 from importlib import import_module
 from os.path import dirname, normpath, sep, basename, relpath, split
 from os import walk
+import inspect
 
 # r=root, d=directories, f = files
+clsmembers = []
 for r, d, f in walk(dirname(__file__)):
     import_path = normpath(relpath(r, split(dirname(__file__))[0])).replace(sep, '.')
     for _file in f:
         if _file.endswith('.py') and not _file.endswith('__init__.py'):
-            import_module('.' + basename(_file)[:-3], import_path)
+            module = import_module('.' + basename(_file)[:-3], import_path)
+            clsmembers.extend(inspect.getmembers(module, inspect.isclass))
