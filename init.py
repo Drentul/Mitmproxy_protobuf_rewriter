658307ed813ce0af116b81020fff08fa697ef378
Artemiy Vanskiy <a.vanskiy@life-stream.tv>
2019-07-09 10:33:40 +0300

    Fixes format of saving protobuf message classes in __init__ file of module

--- init.py
+++ init.py
@@ -5,10 +5,12 @@ import inspect
 
 # r=root, d=directories, f = files
 clsmembers = []
+classes_with_names = []
 for r, d, f in walk(dirname(__file__)):
     import_path = normpath(relpath(r, split(dirname(__file__))[0])).replace(sep, '.')
     for _file in f:
         if _file.endswith('.py') and not _file.endswith('__init__.py'):
             module = import_module('.' + basename(_file)[:-3], import_path)
-            clsmembers.extend(inspect.getmembers(module, inspect.isclass))
-
+            classes_with_names.extend(inspect.getmembers(module, inspect.isclass))
+for cls in classes_with_names:
+    clsmembers.append(cls[1])
