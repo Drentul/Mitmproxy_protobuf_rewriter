8244d48a3efc90bcbfcf74ef0eb33ada6168ebc0
Artemiy Vanskiy <a.vanskiy@life-stream.tv>
2019-07-18 11:14:10 +0300

    Linting file init.py

--- init.py
+++ init.py
@@ -15,12 +15,13 @@ for subdir in subdirs:
 # r=root, d=directories, f = files
 clsmembers = []
 for r, d, f in walk(dirname(__file__)):
-    if '__pycache__' in d: d.remove('__pycache__')
-    if ((r == dirname(__file__)) & (len(d) > 1) & ('example' in d)): d.remove('example')
+    if '__pycache__' in d:
+        d.remove('__pycache__')
+    if ((r == dirname(__file__)) & (len(d) > 1) & ('example' in d)):
+        d.remove('example')
     import_path = normpath(relpath(r, split(dirname(__file__))[0])).replace(sep, '.')
     for _file in f:
         if _file.endswith('.py') and not _file.endswith('__init__.py'):
             module = import_module('.' + basename(_file)[:-3], import_path)
             for class_with_name in inspect.getmembers(module, inspect.isclass):
                 clsmembers.append(class_with_name[1])
-
